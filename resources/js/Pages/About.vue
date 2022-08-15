49bd0045e95318050eb5ef12dad35ba55dc3d192
Ewa <ewa.baker@yahoo.com>
2022-08-15 21:03:51 +0100

    (in 'About' Page) Add team photos and job titles

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -6,7 +6,7 @@
     <header class="text-gray-600 body-font">
       <!-- Company Name and Logo -->
         <div class="container-fluid mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"> 
-            <div class="flex flex-col md:flex-row title-font items-center text-gray-900 mb-4 md:mb-0">                 
+            <div class="flex flex-col md:flex-row title-font items-center text-gray-700">                 
                 <img src="images/bloom_tree.png" alt="" width="90" height="90" /> 
                 <span class="ml-1 text-gray-700 text-4xl lg:text-5xl">BLOOM</span>                               
                 <span class="ml-3 text-gray-700 text-sm md:text-xl">GARDEN DESIGN</span> 
@@ -46,9 +46,9 @@
             </nav>
 
 <!-- About Info -->
-<div class="container-fluid mx-auto p-7 text-gray-700  bg-neutral-50 text-center mt-4 md:text-lg md:my-9 md:mx-5 md:px-0 md:leading-relaxed">
-We are a values-driven, award-winning landscape design practice. We work on a range of projects across the UK encompassing high-end residential, public, historic and commercial spaces. 
-Understanding the spirit of the place is central to our approach, using vernacular materials, nearby nurseries and local craftspeople, so that the garden speaks of and is connected to its broader setting.
+<div class="container-fluid mx-auto p-7 text-gray-700  bg-neutral-50 text-center mt-4 md:text-lg md:my-9 md:mx-5 md:px-0 md:leading-relaxed lg:mx-40">
+<p>We are a values-driven, award-winning landscape design practice. We work on a range of projects across the UK encompassing high-end residential, public, historic and commercial spaces. 
+Understanding the spirit of the place is central to our approach, using vernacular materials, nearby nurseries and local craftspeople, so that the garden speaks of and is connected to its broader setting.</p>
 </div>
 
 <!-- Team Photos-->
@@ -56,115 +56,96 @@ Understanding the spirit of the place is central to our approach, using vernacul
  <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-col text-center w-full mb-20">
-      <h1 class="text-2xl font-medium title-font mb-4 text-gray-900">OUR TEAM</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+      <h1 class="text-2xl font-medium title-font mb-4 text-gray-600">OUR TEAM</h1>      
     </div>
     <div class="flex flex-wrap -m-4">
       <div class="p-4 lg:w-1/4 md:w-1/2">
         <div class="h-full flex flex-col items-center text-center">
-          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://dummyimage.com/200x200">
+          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-100 object-cover object-center mb-4" src="images/teamPhotos/Caroline Damson-Managing Director.jpg">
           <div class="w-full">
-            <h2 class="title-font font-medium text-lg text-gray-900">Alper Kamu</h2>
-            <h3 class="text-gray-500 mb-3">UI Developer</h3>
-            <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
-            <span class="inline-flex">
-              <a class="text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>
-              </a>
-            </span>
+            <h2 class="title-font font-medium text-lg text-gray-700">Caroline Damson</h2>
+            <h3 class="text-gray-500 mb-3">Managing Director</h3>            
+          </div>
+        </div>
+        </div>
+        <div class="p-4 lg:w-1/4 md:w-1/2">
+        <div class="h-full flex flex-col items-center text-center">
+          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-100 object-cover object-center mb-4" src="images/teamPhotos/Amy Concorde-Communications (2).jpg">
+          <div class="w-full">
+            <h2 class="title-font font-medium text-lg text-gray-700">Amy Concorde</h2>
+            <h3 class="text-gray-500 mb-3">Communications</h3>            
+          </div>
+        </div>
+        </div>      
+      <div class="p-4 lg:w-1/4 md:w-1/2">
+        <div class="h-full flex flex-col items-center text-center">
+          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-100 object-cover object-center mb-4" src="images/teamPhotos/Ivy Sunburst-Operations Manager (2).jpg">
+          <div class="w-full">
+            <h2 class="title-font font-medium text-lg text-gray-700">Ivy Sunburst</h2>
+            <h3 class="text-gray-500 mb-3">Operations Manager</h3>            
+          </div>
+        </div>
+        </div>
+        <div class="p-4 lg:w-1/4 md:w-1/2">
+        <div class="h-full flex flex-col items-center text-center">
+          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-100 object-cover object-center mb-4" src="images/teamPhotos/Sam Conference-Lead Designer.jpg">
+          <div class="w-full">
+            <h2 class="title-font font-medium text-lg text-gray-700">Sam Conference</h2>
+            <h3 class="text-gray-500 mb-3">Lead Designer</h3>            
+          </div>
+        </div>
+        </div>
+        <div class="p-4 lg:w-1/4 md:w-1/2">
+        <div class="h-full flex flex-col items-center text-center">
+          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-100 object-cover object-center mb-4" src="images/teamPhotos/Lisa Bramley-Senior Designer (2).jpg">
+          <div class="w-full">
+            <h2 class="title-font font-medium text-lg text-gray-700">Lisa Bramley</h2>
+            <h3 class="text-gray-500 mb-3">Senior Designer</h3>            
+          </div>
+        </div>
+        </div>
+        <div class="p-4 lg:w-1/4 md:w-1/2">
+        <div class="h-full flex flex-col items-center text-center">
+          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-100 object-cover object-center mb-4" src="images/teamPhotos/Luke Rochester-Studio Manager.jpg">
+          <div class="w-full">
+            <h2 class="title-font font-medium text-lg text-gray-700">Luke Rochester</h2>
+            <h3 class="text-gray-500 mb-3">Studio Manager</h3>            
+          </div>
+        </div>
+        </div>
+        <div class="p-4 lg:w-1/4 md:w-1/2">
+        <div class="h-full flex flex-col items-center text-center">
+          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-100 object-cover object-center mb-4" src="images/teamPhotos/Laura Morello-Landscape Architect (2).jpg">
+          <div class="w-full">
+            <h2 class="title-font font-medium text-lg text-gray-700">Laura Morello</h2>
+            <h3 class="text-gray-500 mb-3">Landscape Architect</h3>            
+          </div>
+        </div>
+        </div>
+      <div class="p-4 lg:w-1/4 md:w-1/2">
+        <div class="h-full flex flex-col items-center text-center">
+          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-100 object-cover object-center mb-4" src="images/teamPhotos/Katie Goldcot-Project Manager.jpg">
+          <div class="w-full">
+            <h2 class="title-font font-medium text-lg text-gray-700">Katie Goldcot</h2>
+            <h3 class="text-gray-500 mb-3">Project Manager</h3>          
           </div>
         </div>
       </div>
       <div class="p-4 lg:w-1/4 md:w-1/2">
         <div class="h-full flex flex-col items-center text-center">
-          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://dummyimage.com/201x201">
+          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-100 object-cover object-center mb-4" src="images/teamPhotos/Philip Williams-General Maintenance Manager.jpg">
           <div class="w-full">
-            <h2 class="title-font font-medium text-lg text-gray-900">Holden Caulfield</h2>
-            <h3 class="text-gray-500 mb-3">UI Developer</h3>
-            <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
-            <span class="inline-flex">
-              <a class="text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>
-              </a>
-            </span>
+            <h2 class="title-font font-medium text-lg text-gray-700">Philip Williams</h2>
+            <h3 class="text-gray-500 mb-3">General Maintenance Manager</h3>           
           </div>
         </div>
       </div>
       <div class="p-4 lg:w-1/4 md:w-1/2">
         <div class="h-full flex flex-col items-center text-center">
-          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://dummyimage.com/202x202">
+          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-100 object-cover object-center mb-4" src="images/teamPhotos/Paul Cox-Maintenance Manager.jpg">
           <div class="w-full">
-            <h2 class="title-font font-medium text-lg text-gray-900">Atticus Finch</h2>
-            <h3 class="text-gray-500 mb-3">UI Developer</h3>
-            <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
-            <span class="inline-flex">
-              <a class="text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>
-              </a>
-            </span>
-          </div>
-        </div>
-      </div>
-      <div class="p-4 lg:w-1/4 md:w-1/2">
-        <div class="h-full flex flex-col items-center text-center">
-          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://dummyimage.com/203x203">
-          <div class="w-full">
-            <h2 class="title-font font-medium text-lg text-gray-900">Henry Letham</h2>
-            <h3 class="text-gray-500 mb-3">UI Developer</h3>
-            <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
-            <span class="inline-flex">
-              <a class="text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>
-              </a>
-            </span>
+            <h2 class="title-font font-medium text-lg text-gray-700">Paul Cox</h2>
+            <h3 class="text-gray-500 mb-3">Maintenance Manager</h3>            
           </div>
         </div>
       </div>
